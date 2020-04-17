8c6cb61ecf26fdfd7e199d09efb3657dec4f050c
pop <v.popoff@gmail.com>
2020-04-17 16:56:12 +0300

    parser

--- get_data_map.py
+++ get_data_map.py
@@ -36,9 +36,9 @@ url = 'https://moz.gov.ua/article/news/operativna-informacija-pro-poshirennja-ko
 def getAction():
 	__parser = argparse.ArgumentParser(description='''get day data from MOZ''')
 	__parser.add_argument('--date', type=str, action='store', dest='date', help='date like 01-01-2020', required=True)
-	__parser.add_argument('-c', type=int, action='store', dest='c', help='total sum c', required=True)
-	__parser.add_argument('-d', type=int, action='store', dest='d', help='total sum d', required=True)
-	__parser.add_argument('-r', type=int, action='store', dest='r', help='total sum r', required=True)
+	# __parser.add_argument('-c', type=int, action='store', dest='c', help='total sum c', required=True)
+	# __parser.add_argument('-d', type=int, action='store', dest='d', help='total sum d', required=True)
+	# __parser.add_argument('-r', type=int, action='store', dest='r', help='total sum r', required=True)
 	__parser.add_argument('--file', type=str, action='store', dest='file', help='filename json to save data', required=True)
 	return __parser.parse_args()
 
@@ -57,11 +57,19 @@ def getData(html):
 		return (data, res[0])
 	return list(map(f, area.keys()))
 
-def getDataJson(data, arg):
+def getDataTotal(html):
+	def f(text):
+		patern = re.compile('(\d+) ' + text)
+		res = patern.findall(html)
+		return int(res[0])
+
+	return {'c': f('лабораторно'), 'd': f('летальн'), 'r': f('пацієнт')}
+
+def getDataJson(data, data_total, arg):
 	return {
 		"date": "%s 10:00" % (arg.date),
 		"confirmed": {area[x[0]]:{"c": int(x[1]), "d": 0, "r": 0} for x in data},
-		"total": {"c": arg.c, "d": arg.d, "r": arg.r}
+		"total": {"c": data_total['c'], "d": data_total['d'], "r": data_total['r']}
 	}
 
 def main():
@@ -71,7 +79,10 @@ def main():
 		print('error get html')
 		exit()
 	data = getData(html)
-	data_json = getDataJson(data, __argsActions)
+	data_total = getDataTotal(html)
+	# print(data_total)
+	# exit()
+	data_json = getDataJson(data, data_total, __argsActions)
 
 	f = open(__argsActions.file, 'w')
 	f.write(json.dumps(data_json))
