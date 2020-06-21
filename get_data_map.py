70f8d25d32a3ad9b42a5fbafeec395dcfe344fde
pop <v.popoff@gmail.com>
2020-06-21 11:56:51 +0300

    data

--- get_data_map.py
+++ get_data_map.py
@@ -64,7 +64,7 @@ def getDataTotal(html):
 		res = patern.findall(html)
 		return int(res[0].replace(' ', ''))
 
-	return {'c': f('лаборат'), 'd': f('леталь'), 'r': f('пацієнт')}
+	return {'c': f('випадк'), 'd': f('померл'), 'r': f('одужа')}
 
 def getDataJson(data, data_total, arg):
 	return {
